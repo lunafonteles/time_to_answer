222688abdfbe347317e75e403ab5e1a8ff5f53e6
lunafonteles <luna.fonteles@vicegov.ce.gov.br>
2024-05-16 14:46:10 -0300

    ajustando estrutura base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -7,10 +10,9 @@ Rails.application.routes.draw do
   end
   devise_for :profiles
   devise_for :admins
-  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
   # root "articles#index"
 end
